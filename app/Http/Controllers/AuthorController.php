8a57557e7aa5bd9bba52707ea8b329abd00472d1
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-11-14 17:02:28 +0800

    FETCHING AUTHORS

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -11,7 +11,8 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        //
+        $authors = Author::all(); 
+        return view('admin.authors', compact('authors'));
     }
 
     /**
--- resources/views/admin/authors.blade.php
+++ resources/views/admin/authors.blade.php
@@ -1,3 +1,160 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+@extends('components.default')
+
+@section('title', 'Authors | Admin Dashboard | LMIS')
+
+@section('content')
+
+<section>
+    <div class="min-h-screen pt-24">
+        {{-- @include('components.admin.bg') --}}
+        {{-- Include Top Navigation --}}
+        @include('components.admin.topnav')
+        <div class="flex flex-col lg:flex-row px-4 lg:px-10 pb-4 gap-6">
+
+            {{-- Include Sidebar --}}
+            <div class="lg:w-2/12 w-full">
+                @include('components.admin.sidebar')
+            </div>
+
+            {{-- Main Content --}}
+            <div class="lg:w-10/12 w-full">
+
+                <div class="bg-white rounded-xl shadow-lg">
+                    <div class="px-6 py-6">
+
+
+                        <!-- Breadcrumb -->
+                        <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+                            aria-label="Breadcrumb">
+                            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                                <li class="inline-flex items-center">
+                                    <a href="#"
+                                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                        <svg class="w-3 h-3 me-2.5" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path
+                                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                                        </svg>
+                                        Admin
+                                    </a>
+                                </li>
+                                <li>
+                                    <div class="flex items-center">
+                                        <svg class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 " aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m1 9 4-4-4-4" />
+                                        </svg>
+                                        <a href="#"
+                                            class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Dashboard</a>
+                                    </div>
+                                </li>
+                                <li aria-current="page">
+                                    <div class="flex items-center">
+                                        <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m1 9 4-4-4-4" />
+                                        </svg>
+                                        <span
+                                            class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Authors</span>
+                                    </div>
+                                </li>
+                            </ol>
+                        </nav>
+
+                    </div>
+
+                    <div class="relative overflow-x-auto sm:rounded-lg  px-6 py-6">
+                        <table id="datatable"
+                            class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 py-4">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Author's Name
+                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                    viewBox="0 0 24 24">
+                                                    <path
+                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg></a>
+                                        </div>
+                                    </th>
+
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Actions
+                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                    viewBox="0 0 24 24">
+                                                    <path
+                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg></a>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($authors as $author)
+                                <tr class="odd:bg-white even:bg-gray-50 border-b py-4">
+                                    <td class="px-6 py-4">{{ $author->name }}</td>
+                                    <td class="px-6 py-4 flex gap-2">
+                                        {{-- Edit --}}
+                                        <a href="{{ route('authors.edit', $author->id) }}"
+                                            class="px-3 py-2 text-xs text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                                            Edit
+                                        </a>
+
+                                        {{-- Delete --}}
+                                        <button data-id="{{ $author->id }}"
+                                            class="delete-author-btn px-3 py-2 text-xs text-white bg-red-600 rounded-lg hover:bg-red-700">
+                                            Delete
+                                        </button>
+
+                                        <form id="delete-author-form-{{ $author->id }}"
+                                            action="{{ route('authors.destroy', $author->id) }}" method="POST"
+                                            class="hidden">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+@push('scripts')
+@include('components.alerts')
+@push('scripts')
+<script>
+    document.querySelectorAll('.delete-author-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        let authorId = this.getAttribute('data-id');
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "This author will be deleted permanently!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById(`delete-author-form-${authorId}`).submit();
+            }
+        });
+    });
+});
+</script>
+@endpush
+@endpush
