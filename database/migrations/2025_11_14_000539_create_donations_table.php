689d9dd6b3a50245e4f8d3c4eda4ba64516e7d20
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-11-20 15:08:17 +0800

    FIX SEEDERS

--- database/migrations/2025_11_14_000539_create_donations_table.php
+++ database/migrations/2025_11_14_000539_create_donations_table.php
@@ -14,12 +14,17 @@ return new class extends Migration
         Schema::create('donations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('donor_id')->constrained('users')->onDelete('cascade');
+
             $table->string('book_title');
-            $table->foreignId('author_id')->after('book_title')->constrained('authors')->onDelete('cascade');
-            $table->foreignId('publisher_id')->after('author_id')->constrained('publishers')->onDelete('cascade');
+            $table->foreignId('author_id')->constrained('authors')->onDelete('cascade');
+            $table->foreignId('publisher_id')->constrained('publishers')->onDelete('cascade');
+
             $table->integer('year_published')->nullable();
             $table->integer('quantity')->default(1);
-            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+
+            $table->enum('status', ['pending', 'accepted', 'rejected'])
+                ->default('pending');
+
             $table->timestamps();
         });
     }
