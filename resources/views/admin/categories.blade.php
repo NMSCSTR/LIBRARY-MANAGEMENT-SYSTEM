d24f4fc9d8214e5d0b6596286350c3e3adde4476
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2026-01-12 14:41:05 +0800

    make this light theme

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -3,157 +3,91 @@
 @section('content')
 
 <section>
-    <div class="min-h-screen pt-24">
-        {{-- @include('components.admin.bg') --}}
-        {{-- Include Top Navigation --}}
+    <div class="min-h-screen pt-24 bg-gray-100">
         @include('components.admin.topnav')
-        <div class="flex flex-col lg:flex-row px-4 lg:px-10 pb-4 gap-6">
 
-            {{-- Include Sidebar --}}
+        <div class="flex flex-col lg:flex-row px-4 lg:px-10 pb-6 gap-6">
+
+            {{-- Sidebar --}}
             <div class="lg:w-2/12 w-full">
                 @include('components.admin.sidebar')
             </div>
 
             {{-- Main Content --}}
             <div class="lg:w-10/12 w-full">
+                <div class="bg-white rounded-xl shadow-md">
 
-                <div class="bg-white rounded-xl shadow-lg">
+                    {{-- Header --}}
                     <div class="px-6 py-6">
 
-
-                        <!-- Breadcrumb -->
-                        <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
-                            aria-label="Breadcrumb">
-                            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                                <li class="inline-flex items-center">
-                                    <a href="#"
-                                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                                        <svg class="w-3 h-3 me-2.5" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                            <path
-                                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                                        </svg>
+                        {{-- Breadcrumb --}}
+                        <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-100">
+                            <ol class="inline-flex items-center space-x-2">
+                                <li>
+                                    <a href="#" class="text-sm font-medium text-gray-700 hover:text-blue-600">
                                         Admin
                                     </a>
                                 </li>
+                                <li class="text-gray-400">/</li>
                                 <li>
-                                    <div class="flex items-center">
-                                        <svg class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 " aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2" d="m1 9 4-4-4-4" />
-                                        </svg>
-                                        <a href="#"
-                                            class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Dashboard</a>
-                                    </div>
+                                    <a href="#" class="text-sm font-medium text-gray-700 hover:text-blue-600">
+                                        Dashboard
+                                    </a>
                                 </li>
-                                <li aria-current="page">
-                                    <div class="flex items-center">
-                                        <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2" d="m1 9 4-4-4-4" />
-                                        </svg>
-                                        <span
-                                            class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Categories</span>
-                                    </div>
+                                <li class="text-gray-400">/</li>
+                                <li class="text-sm font-medium text-gray-500">
+                                    Categories
                                 </li>
                             </ol>
                         </nav>
 
-
-                        <div class="flex justify-end py-4">
-                            <button id="defaultModalButton" data-modal-target="defaultModal"
-                                data-modal-toggle="defaultModal" class="flex items-center gap-2 text-white bg-blue-600 hover:bg-blue-700
-                                    focus:ring-4 focus:outline-none focus:ring-blue-300
-                                    font-medium rounded-lg text-sm px-5 py-2.5 shadow-md hover:shadow-lg transition">
-
-                                <!-- Plus Icon -->
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                    stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        {{-- Add Button --}}
+                        <div class="flex justify-end mt-4">
+                            <button
+                                data-modal-target="defaultModal"
+                                data-modal-toggle="defaultModal"
+                                class="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium px-5 py-2.5 rounded-lg shadow">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M12 4.5v15m7.5-7.5h-15" />
                                 </svg>
-
-                                Add Categories
+                                Add Category
                             </button>
                         </div>
-
                     </div>
 
-                    <div class="relative overflow-x-auto sm:rounded-lg  px-6 py-6">
-                        <table id="datatable"
-                            class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 py-4">
+                    {{-- Table --}}
+                    <div class="relative overflow-x-auto px-6 pb-6">
+                        <table class="w-full text-sm text-left text-gray-600">
+                            <thead class="text-xs uppercase bg-gray-100 text-gray-700">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3">
-                                        <div class="flex items-center">
-                                            Name
-                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path
-                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                                </svg></a>
-                                        </div>
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        <div class="flex items-center">
-                                            Description
-                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path
-                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                                </svg></a>
-                                        </div>
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        <div class="flex items-center">
-                                            Number of Books
-                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path
-                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                                </svg></a>
-                                        </div>
-                                    </th>
-
-                                    <th scope="col" class="px-6 py-3">
-                                        <div class="flex items-center">
-                                            Actions
-                                            <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path
-                                                        d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                                </svg></a>
-                                        </div>
-                                    </th>
+                                    <th class="px-6 py-3">Name</th>
+                                    <th class="px-6 py-3">Description</th>
+                                    <th class="px-6 py-3">Books</th>
+                                    <th class="px-6 py-3">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($categories as $category)
-                                <tr class="odd:bg-white even:bg-gray-50 border-b">
+                                <tr class="odd:bg-white even:bg-gray-50 border-b border-gray-200">
                                     <td class="px-6 py-4">{{ $category->name }}</td>
                                     <td class="px-6 py-4">{{ $category->description ?? '-' }}</td>
                                     <td class="px-6 py-4">{{ $category->books_count }}</td>
                                     <td class="px-6 py-4 flex gap-2">
-                                        {{-- Edit --}}
                                         <a href="{{ route('categories.edit', $category->id) }}"
-                                            class="px-3 py-2 text-xs text-white bg-blue-700 hover:bg-blue-800">
+                                            class="px-3 py-2 text-xs text-white bg-blue-600 hover:bg-blue-700 rounded-md">
                                             Edit
                                         </a>
-                                        {{-- Delete --}}
+
                                         <button data-id="{{ $category->id }}"
-                                            class="delete-category-btn px-3 py-2 text-xs text-white bg-red-600 hover:bg-red-700">
+                                            class="delete-category-btn px-3 py-2 text-xs text-white bg-red-500 hover:bg-red-600 rounded-md">
                                             Delete
                                         </button>
 
                                         <form id="delete-category-form-{{ $category->id }}"
-                                            action="{{ route('categories.destroy', $category->id) }}" method="POST"
-                                            class="hidden">
+                                            action="{{ route('categories.destroy', $category->id) }}"
+                                            method="POST" class="hidden">
                                             @csrf
                                             @method('DELETE')
                                         </form>
@@ -162,49 +96,47 @@
                                 @endforeach
                             </tbody>
                         </table>
-
-                        </table>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
-    <!-- Add Category Modal -->
-    <div id="defaultModal" tabindex="-1" aria-hidden="true"
-        class="hidden fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
-        <div class="relative w-full max-w-xl p-4">
-            <div class="relative bg-white rounded-2xl shadow-2xl p-6">
 
-                {{-- Modal Header --}}
+    {{-- Modal --}}
+    <div id="defaultModal" class="hidden fixed inset-0 z-50 flex items-center justify-center bg-black/30">
+        <div class="w-full max-w-xl p-4">
+            <div class="bg-white rounded-2xl shadow-xl p-6">
+
                 <div class="flex items-center justify-between border-b pb-3 mb-4">
-                    <h3 class="text-xl font-bold text-gray-800 flex items-center gap-2">
+                    <h3 class="text-lg font-semibold text-gray-800">
                         Add New Category
                     </h3>
-                    <button type="button" class="p-2 rounded-full hover:bg-gray-200 text-gray-500"
-                        data-modal-toggle="defaultModal">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+                    <button data-modal-toggle="defaultModal"
+                        class="text-gray-400 hover:text-gray-600">✕</button>
                 </div>
 
-                {{-- Modal Body --}}
                 <form action="{{ route('categories.store') }}" method="POST" class="space-y-4">
                     @csrf
-                    <div class="space-y-1">
-                        <label class="text-sm font-medium text-gray-700">Category Name</label>
+
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700 mb-1">
+                            Category Name
+                        </label>
                         <input type="text" name="name" required
-                            class="w-full p-2.5 bg-gray-50 rounded-lg">
+                            class="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none">
                     </div>
-                    <div class="space-y-1">
-                        <label class="text-sm font-medium text-gray-700">Description</label>
-                        <textarea name="description"
-                            class="w-full p-2.5 bg-gray-50 rounded-lg"></textarea>
+
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700 mb-1">
+                            Description
+                        </label>
+                        <textarea name="description" rows="3"
+                            class="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"></textarea>
                     </div>
 
                     <button type="submit"
-                        class="w-full bg-blue-600 text-white py-2.5 rounded-lg font-medium hover:bg-blue-700 transition">
+                        class="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg px-5 py-2.5 shadow">
                         Add Category
                     </button>
                 </form>
@@ -214,28 +146,27 @@
     </div>
 </section>
 @endsection
+
 @push('scripts')
 @include('components.alerts')
-@push('scripts')
 <script>
     document.querySelectorAll('.delete-category-btn').forEach(button => {
         button.addEventListener('click', function () {
-            let categoryId = this.getAttribute('data-id');
+            let id = this.dataset.id;
             Swal.fire({
                 title: 'Are you sure?',
-                text: "This category will be deleted permanently!",
+                text: 'This category will be deleted permanently!',
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#3085d6',
+                confirmButtonColor: '#dc2626',
+                cancelButtonColor: '#2563eb',
                 confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
+            }).then(result => {
                 if (result.isConfirmed) {
-                    document.getElementById(`delete-category-form-${categoryId}`).submit();
+                    document.getElementById(`delete-category-form-${id}`).submit();
                 }
             });
         });
     });
 </script>
-@endpush
 @endpush
