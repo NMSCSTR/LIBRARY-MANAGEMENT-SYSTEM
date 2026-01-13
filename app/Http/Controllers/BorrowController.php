1b159e6b80881d305814850bbbfe37a8d2a001ef
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2026-01-13 14:02:01 +0800

    changing workflows on borrows ss

--- app/Http/Controllers/BorrowController.php
+++ app/Http/Controllers/BorrowController.php
@@ -6,6 +6,7 @@ use App\Models\Book;
 use App\Models\BookCopy;
 use App\Models\Borrow;
 use App\Models\User;
+use App\Models\Reservation;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
